f480cab108e492d9b30b926097eb97fb27d96fc3
Abelardo Palácios <71283403+AbelardoOk@users.noreply.github.com>
2023-08-10 21:18:38 -0400

    feat: First Commit

---
+++ tailwind.config.ts
@@ -0,0 +1,35 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "var(--font-ubuntu)",
+      },
+
+      colors: {
+        neutral: {
+          coolGray: "hsl(231, 11%, 63%)",
+          lightGray: "hsl(229, 24%, 87%)",
+          magnolia: "hsl(217, 100%, 97%)",
+          alabaster: "hsl(231, 100%, 99%)",
+        },
+
+        primary: {
+          marineBlue: "hsl(213, 96%, 18%)",
+          purplishBlue: "hsl(243, 100%, 62%)",
+          pastelBlue: "hsl(228, 100%, 84%)",
+          lightBlue: "hsl(206, 94%, 87%)",
+          red: "hsl(354, 84%, 57%)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
